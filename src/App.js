ab2b2386ed2186b19c1f5beca9a4483d28a238c3
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-05-14 20:12:03 +0530

    react advice app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'; //Axios is a popular, promise-based HTTP client that sports an easy-to-use API and can be used in both the browser and Node.js.
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    advice: ''
+  };
+  componentDidMount() {
+    this.fetchadvice()
+  }
+  fetchadvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const {advice} = response.data.slip
+        this.setState({advice})
+      //  document.write(response.data.slip.advice)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  render() {
+    const {advice} = this.state
+    return (
+      <div className="App">
+        <div className="card">
+        <h1 className="heading"> {advice}</h1>
+        </div> 
+        <button className="button" onClick={this.fetchadvice}>
+          <span> Give me advice </span>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
